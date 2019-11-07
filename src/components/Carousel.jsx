404ae5ed6c30584cf0de1605bf009e17b304ad3a
dcorn068 <dcorn068@users.noreply.github.com>
2019-11-06 21:47:00 -0500

    implement carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,19 +1,54 @@
-import React from "react"
+import React, { useState } from "react"
 import styled from "styled-components/macro"
 import { useStaticQuery } from "gatsby"
 import Img from "gatsby-image"
 import { imagesQuery } from "./constants"
+import ArrowLeftIcon from "@material-ui/icons/ArrowBackIos"
+import ArrowRightIcon from "@material-ui/icons/ArrowForwardIos"
+import { IconButton } from "@material-ui/core"
+import { animated, useSpring } from "react-spring"
+
+const CAROUSEL_MAX_WIDTH = 960
 
 const CarouselStyles = styled.div`
-  display: grid;
-  grid-auto-flow: column;
+  max-width: ${CAROUSEL_MAX_WIDTH}px;
+  max-height: 100vh;
+  margin: auto;
+  .images-wrapper {
+    display: grid;
+    grid-auto-flow: column;
+  }
   .img-wrapper {
+    max-width: ${CAROUSEL_MAX_WIDTH}px;
+    width: 100vw;
+    img {
+      width: 100%;
+    }
+  }
+  position: relative;
+  .arrow-wrapper {
+    position: absolute;
+    z-index: 1;
+    height: 100%;
+    display: grid;
+    align-items: center;
+    &.arrow-right {
+      right: 1em;
+    }
+    &.arrow-left {
+      left: 1em;
+    }
+    .MuiIconButton-root {
+      color: rgba(255, 255, 255, 0.54);
+    }
+    .MuiButtonBase-root.MuiIconButton-root:hover {
+      background-color: rgba(255, 255, 255, 0.16);
+    }
   }
 `
 
 export default () => {
   const data = useStaticQuery(imagesQuery)
-  console.log("⚡🚨: data", data)
 
   const images = [
     data.image1.childImageSharp.fluid,
@@ -24,13 +59,42 @@ export default () => {
     data.image6.childImageSharp.fluid,
   ]
 
+  const [selectedImgIndex, setSelectedImgIndex] = useState(0)
+
+  const carouselWidth = Math.min(window.innerWidth, CAROUSEL_MAX_WIDTH)
+
+  const handlePrevious = () => {
+    const prevIndex = selectedImgIndex - 1
+    setSelectedImgIndex(prevIndex < 0 ? images.length - 1 : prevIndex)
+  }
+  const handleNext = () => {
+    const nextIndex = selectedImgIndex + 1
+    setSelectedImgIndex(nextIndex > images.length - 1 ? 0 : nextIndex)
+  }
+
+  const springLeftRight = useSpring({
+    transform: `translate(${-selectedImgIndex * carouselWidth}px,0)`,
+  })
+
   return (
     <CarouselStyles>
-      {images.map((image, idx) => (
-        <div className="img-wrapper">
-          <Img key={idx} title={"hi"} fluid={image} />
-        </div>
-      ))}
+      <div className="arrow-wrapper arrow-left">
+        <IconButton onClick={handlePrevious}>
+          <ArrowLeftIcon />
+        </IconButton>
+      </div>
+      <animated.div className="images-wrapper" style={springLeftRight}>
+        {images.map((image, idx) => (
+          <div className="img-wrapper">
+            <Img key={idx} title={"hi"} fluid={image} />
+          </div>
+        ))}
+      </animated.div>
+      <div className="arrow-wrapper arrow-right">
+        <IconButton onClick={handleNext}>
+          <ArrowRightIcon />
+        </IconButton>
+      </div>
     </CarouselStyles>
   )
 }
