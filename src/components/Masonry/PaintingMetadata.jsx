1ef6e77365c34fe1262df0092be8cadffb15eb37
dcorn068 <daniel@raventelemetry.com>
2019-12-26 17:44:04 -0500

    trim width/height numbers

--- src/components/Masonry/PaintingMetadata.jsx
+++ src/components/Masonry/PaintingMetadata.jsx
@@ -18,11 +18,12 @@ export default ({ metadata }) => {
   const { heightInches, widthInches, depthInches, title, type } = metadata
   const heightTrimmed = Number(heightInches.toFixed(2))
   const widthTrimmed = Number(widthInches.toFixed(2))
+  const depthTrimmed = Number(depthInches.toFixed(2))
   return (
     <MetadataStyles>
       <div className="title">{title}</div>
       <div className="details">
-        {type}, {heightTrimmed} H x {widthTrimmed} W x {depthInches} in
+        {type}, {heightTrimmed} H x {widthTrimmed} W x {depthTrimmed} in
       </div>
     </MetadataStyles>
   )
