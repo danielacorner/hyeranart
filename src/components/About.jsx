b51b25e05146b155663c05cd54df347755f028ab
dcorn068 <dcorn068@users.noreply.github.com>
2020-01-13 23:45:29 -0500

    about styles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,9 @@
 import React from "react"
-
+import styled from "styled-components/macro"
+const AboutStyles = styled.div`
+  width: fit-content;
+  margin: 2em 5em 0 auto;
+`
 export default () => {
-  return <div>Under construction</div>
+  return <AboutStyles>Under construction</AboutStyles>
 }
