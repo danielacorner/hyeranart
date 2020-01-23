e83c861d66a8d04730c5564344713b3ac0d5381d
dcorn068 <dcorn068@users.noreply.github.com>
2020-01-23 00:45:52 -0500

    fix build

--- src/templates/sectionTemplate.jsx
+++ src/templates/sectionTemplate.jsx
@@ -3,8 +3,6 @@ import Layout from "../components/Layout"
 import styled from "styled-components"
 import { useMediaQuery } from "@material-ui/core"
 import { BREAKPOINTS } from "../utils/constants"
-import { animated } from "react-spring"
-import { useTransitionUpDown } from "./collectionTemplate"
 
 const SectionStyles = styled.div`
   padding-top: 70px;
@@ -22,47 +20,38 @@ const SectionStyles = styled.div`
     width: 100%;
     height: auto;
   }
-    max-width: 960px;
-    margin-bottom: 6em;
+  max-width: 960px;
+  margin-bottom: 6em;
 `
 
 export default function Template({ pageContext }) {
   const { title, moreInfo } = pageContext
   const isMobileOrLarger = useMediaQuery(`(min-width: ${BREAKPOINTS.MOBILE}px)`)
 
-  const transitions = useTransitionUpDown()
-
   return (
     <Layout>
-      {transitions.map(
-        ({ item, key, props }) =>
-          item && (
-            <animated.div key={key} style={props}>
-              <SectionStyles>
-                  <div
-                    style={{
-                      display: "flex",
-                      flexDirection: isMobileOrLarger ? "row" : "column",
-                      alignItems: "baseline",
-                    }}
-                  >
-                    <h1
-                      style={{
-                        flexGrow: 1,
-                        ...(isMobileOrLarger ? {} : { marginBottom: "0.5em" }),
-                      }}
-                    >
-                      {title}
-                    </h1>
-                  </div>
-                  <div
-                    className="sectionInfo"
-                    dangerouslySetInnerHTML={{ __html: moreInfo }}
-                  />
-              </SectionStyles>
-            </animated.div>
-          )
-      )}
+      <SectionStyles>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: isMobileOrLarger ? "row" : "column",
+            alignItems: "baseline",
+          }}
+        >
+          <h1
+            style={{
+              flexGrow: 1,
+              ...(isMobileOrLarger ? {} : { marginBottom: "0.5em" }),
+            }}
+          >
+            {title}
+          </h1>
+        </div>
+        <div
+          className="sectionInfo"
+          dangerouslySetInnerHTML={{ __html: moreInfo }}
+        />
+      </SectionStyles>
     </Layout>
   )
 }
