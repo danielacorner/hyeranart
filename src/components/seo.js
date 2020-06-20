f13ea1d6037bd0894ac3fc42cc682d4e4368bd06
dcorn068 <daniel@raventelemetry.com>
2020-06-20 09:47:33 -0400

    increase favicon size

--- src/components/seo.js
+++ src/components/seo.js
@@ -68,7 +68,9 @@ function SEO({ description, lang, meta, title }) {
           content: metaDescription,
         },
       ].concat(meta)}
-    />
+    >
+      <link rel="icon" href={favicon} />
+    </Helmet>
   )
 }
 
